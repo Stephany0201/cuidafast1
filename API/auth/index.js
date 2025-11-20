a878ff5b2d5068e2f243a19be4d99fb5241cdbfc
cuidafast-sketch <cuidafast@gmail.com>
2025-11-20 18:40:29 -0300

    Create index.js

---
+++ API/auth/index.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Método não permitido' });
+  }
+  return res.status(200).json({ ok: true, rota: 'auth funcionando! kkk' });
+}
