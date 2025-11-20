c2ba5ccf7f253b7fb57d86e334f69f79db2cc75c
carol licciardi <carolzinha2800@gmail.com>
2025-11-19 22:26:12 -0300

    oiiiiiiiiii

--- back-end/dbSupabase.js
+++ back-end/dbSupabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL="https://omvwicetojhqurdeuequ.supabase.co",
-  process.env.SUPABASE_SERVICE_ROLE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tdndpY2V0b2pocXVyZGV1ZXF1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzQxMjkxMSwiZXhwIjoyMDc4OTg4OTExfQ.eYhsNaqRrQy0xWLEg-rbHPKJFq5JqhfPoOVjQNXPX3U"
+  process.env.SUPABASE_URL="https://kgwepkcxmsoyebxczqwe.supabase.co",
+  process.env.SUPABASE_SERVICE_ROLE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtnd2Vwa2N4bXNveWVieGN6cXdlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTI0ODEyMSwiZXhwIjoyMDc2ODI0MTIxfQ.B41deTxaC_Xv0Csb9Dsd8voBBVuzrTKwoQdBhUlV39A"
 );
