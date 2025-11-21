c9be61df8eb144ae75b2113ff6ef9c0f095f1d90
carol licciardi <carolzinha2800@gmail.com>
2025-11-21 15:47:48 -0300

    Delete back-end/api/routes directory

--- back-end/api/routes/clienteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const clienteController = require('../controllers/clienteController');
-
-// Salvar dados complementares do cliente (endereço, CPF, etc)
-router.post('/dados-complementares', clienteController.salvarDadosComplementares);
-
-module.exports = router;
--- back-end/api/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DashboardController = require('../controllers/dashboardController');
-
-router.get('/dashboard', DashboardController.getDashboard);
-
-module.exports = router;
-
