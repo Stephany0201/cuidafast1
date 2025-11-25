097e0dfc27b646de024111bc84eb3f618bf89029
cuidafast-sketch <cuidafast@gmail.com>
2025-11-25 12:42:11 -0300

    Update dados.js

--- api/cliente/dados.js
+++ api/cliente/dados.js
@@ -1,9 +1,9 @@
-const clienteController = require('../../back-end/api/controllers/clienteController');
+//const clienteController = require('../../back-end/api/controllers/clienteController');
 
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
+//module.exports = async (req, res) => {
+  //if (req.method !== 'POST') {
+    //return res.status(405).json({ error: 'Method Not Allowed' });
+  //}
 
-  return clienteController.salvarDadosComplementares(req, res);
-};
+  //return clienteController.salvarDadosComplementares(req, res);
+//};
