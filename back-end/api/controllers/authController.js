247f167439afad6341135e5638ae15851c75e252
cuidafast-sketch <carolzinha2800@gmail.com>
2025-11-27 16:11:35 -0300

    a

--- back-end/api/controllers/authController.js
+++ back-end/api/controllers/authController.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { createClient } from '@supabase/supabase-js';
 
 import UsuarioModel from '../models/UsuarioModel.js';
