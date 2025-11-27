f373580ebadf88a80be2318521c898f833495efc
carol licciardi <carolzinha2800@gmail.com>
2025-11-27 13:24:15 -0300

    mercado pagoooo

---
+++ back-end/api/controllers/pagamentoController.js
@@ -0,0 +1,21 @@
+import { criarPreferenciaPagamento } from "../services/mercadoPagoService.js";
+
+export async function criarPagamento(req, res) {
+  const { valor, descricao, idUsuario } = req.body;
+
+  if (!valor || !descricao) {
+    return res.status(400).json({ message: "Dados incompletos" });
+  }
+
+  const result = await criarPreferenciaPagamento({
+    valor,
+    descricao,
+    idUsuario
+  });
+
+  if (result.error) {
+    return res.status(500).json(result);
+  }
+
+  return res.status(200).json(result);
+}
