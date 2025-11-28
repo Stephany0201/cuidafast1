fc4ea6d50a634aa48489c588cb145c7d6218852f
carol licciardi <carolzinha2800@gmail.com>
2025-11-28 08:15:28 -0300

    Delete Downloads/cuidafast1-main/cuidafast1-main directory

--- Downloads/cuidafast1-main/cuidafast1-main/api/cliente/dados.js
+++
@@ -1,9 +0,0 @@
-//const clienteController = require('../../back-end/api/controllers/clienteController');
-
-//module.exports = async (req, res) => {
-  //if (req.method !== 'POST') {
-    //return res.status(405).json({ error: 'Method Not Allowed' });
-  //}
-
-  //return clienteController.salvarDadosComplementares(req, res);
-//};
